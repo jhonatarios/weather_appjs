448010ee61b019c488b19ac67f3b449f2cfae157
Jhonata Rios <72048481+jhonatarios@users.noreply.github.com>
2022-09-14 10:32:21 -0300

    34min

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Variables and elements selection
+
+const apiKey= "b97aaa49d7d96bb8a05b1b5a1bf69d37";
+const apiCountryApi = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+// Functions
+
+// Events
